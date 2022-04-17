76c925ca023f25508bca83cec271213307dd32cb
106368030chentairong <t106368030@ntut.edu.tw>
2022-04-17 21:57:58 +0800

    update

--- cm_web3/main.js
+++ cm_web3/main.js
@@ -1,49 +1,20 @@
+// 添加此至 javascript 中，讓程式在一載入就先判斷有沒有 web3 如果沒有的話我們採用 infura 主網的節點
+window.onload = function() {
 
-window.userWalletAddress = null
-const loginButton = document.getElementById('loginButton')
-const userWallet = document.getElementById('userWallet')
+    const {ethereum} =window;
 
-function toggleButton() {
-  if (!window.ethereum) {
-    loginButton.innerText = 'MetaMask is not installed'
-    loginButton.classList.remove('bg-purple-500', 'text-white')
-    loginButton.classList.add('bg-gray-500', 'text-gray-100', 'cursor-not-allowed')
-    return false
+    const ethereumButton = document.querySelector('.enableEthereumButton');
+    const showAccount = document.querySelector('.showAccount');
+    const address = await window.ethereum.enable();
+    console.log(address)
+    ethereumButton.addEventListener('click', () => {
+    getAccount();
+    });
+
+    async function getAccount() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    showAccount.innerHTML = account;
+    }
+
   }
-
-  loginButton.addEventListener('click', loginWithMetaMask)
-}
-
-async function loginWithMetaMask() {
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    .catch((e) => {
-      console.error(e.message)
-      return
-    })
-  if (!accounts) { return }
-
-  window.userWalletAddress = accounts[0]
-  userWallet.innerText = window.userWalletAddress
-  loginButton.innerText = 'Sign out of MetaMask'
-
-  loginButton.removeEventListener('click', loginWithMetaMask)
-  setTimeout(() => {
-    loginButton.addEventListener('click', signOutOfMetaMask)
-  }, 200)
-}
-
-function signOutOfMetaMask() {
-  window.userWalletAddress = null
-  userWallet.innerText = ''
-  loginButton.innerText = 'Sign in with MetaMask'
-
-  loginButton.removeEventListener('click', signOutOfMetaMask)
-  setTimeout(() => {
-    loginButton.addEventListener('click', loginWithMetaMask)
-  }, 200)
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  toggleButton()
-});
-
