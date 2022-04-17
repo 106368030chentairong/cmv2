33d746318eb47a145b472e41fd65bc5bc05d92b2
106368030chentairong <t106368030@ntut.edu.tw>
2022-04-17 22:29:58 +0800

    fix

--- cm_web3/main.js
+++ cm_web3/main.js
@@ -1,50 +1,50 @@
+window.onload=function (){
+  window.userWalletAddress = null
+  const loginButton = document.getElementById('loginButton')
+  const userWallet = document.getElementById('userWallet')
 
-window.userWalletAddress = null
-const loginButton = document.getElementById('loginButton')
-const userWallet = document.getElementById('userWallet')
+  function toggleButton() {
+    if (!window.ethereum) {
+      loginButton.innerText = 'MetaMask is not installed'
+      loginButton.classList.remove('bg-purple-500', 'text-white')
+      loginButton.classList.add('bg-gray-500', 'text-gray-100', 'cursor-not-allowed')
+      return false
+    }
 
-function toggleButton() {
-  if (!window.ethereum) {
-    loginButton.innerText = 'MetaMask is not installed'
-    loginButton.classList.remove('bg-purple-500', 'text-white')
-    loginButton.classList.add('bg-gray-500', 'text-gray-100', 'cursor-not-allowed')
-    return false
+    loginButton.addEventListener('click', loginWithMetaMask)
   }
 
-  loginButton.addEventListener('click', loginWithMetaMask)
-}
+  async function loginWithMetaMask() {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      .catch((e) => {
+        console.error(e.message)
+        return
+      })
+    if (!accounts) { return }
 
-async function loginWithMetaMask() {
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    .catch((e) => {
-      console.error(e.message)
-      return
-    })
-  if (!accounts) { return }
+    window.userWalletAddress = accounts[0]
+    userWallet.innerText = window.userWalletAddress
+    loginButton.innerText = 'Sign out of MetaMask'
 
-  window.userWalletAddress = accounts[0]
-  userWallet.innerText = window.userWalletAddress
-  loginButton.innerText = 'Sign out of MetaMask'
+    loginButton.removeEventListener('click', loginWithMetaMask)
+    setTimeout(() => {
+      loginButton.addEventListener('click', signOutOfMetaMask)
+    }, 200)
+  }
 
-  loginButton.removeEventListener('click', loginWithMetaMask)
-  setTimeout(() => {
-    loginButton.addEventListener('click', signOutOfMetaMask)
-  }, 200)
-}
+  function signOutOfMetaMask() {
+    window.userWalletAddress = null
+    userWallet.innerText = ''
+    loginButton.innerText = 'Sign in with MetaMask'
 
-function signOutOfMetaMask() {
-  window.userWalletAddress = null
-  userWallet.innerText = ''
-  loginButton.innerText = 'Sign in with MetaMask'
+    loginButton.removeEventListener('click', signOutOfMetaMask)
+    setTimeout(() => {
+      loginButton.addEventListener('click', loginWithMetaMask)
+    }, 200)
+  }
 
-  loginButton.removeEventListener('click', signOutOfMetaMask)
-  setTimeout(() => {
-    loginButton.addEventListener('click', loginWithMetaMask)
-  }, 200)
-}
+  window.addEventListener('DOMContentLoaded', () => {
+    toggleButton()
+  });
 
-window.addEventListener('DOMContentLoaded', () => {
-  toggleButton()
-});
-
-
+};
